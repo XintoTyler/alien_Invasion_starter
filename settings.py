befc99fc687295456733cb1ef4b55639836f4d2d
Tyler Fowsantia <tfowsantia.student.cscc.edu>
2025-11-16 21:46:30 -0500

    Creating AlienInvasion game, objects, and settings

---
+++ settings.py
@@ -0,0 +1,20 @@
+class Settings:
+    """A class to store all settings for AlienInvasion Game."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (90, 90, 90)
+
+        # Ship settings
+        self.ship_speed = 3.0
+
+        # Bullet settings
+        self.bullet_speed = 5.0
+        self.bullet_width = 3
+        self. bullet_height = 15
+        self.bullet_color = (230, 230, 230)
+        self.bullets_allowed = 5
