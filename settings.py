0e871c31b5200355e4e7be0f3757731ec9a32b30
Tyler Fowsantia <tfowsantia.student.cscc.edu>
2025-11-23 21:05:43 -0500

    Added files alien, game_stats, updates on collision checks, game state features, alien fleet spawning and moving, and changes to alien fleet spawning pattern

--- settings.py
+++ settings.py
@@ -5,12 +5,15 @@ class Settings:
         """Initialize the game's settings."""
 
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        # Tyler note: I made my screen smaller because of the image i was using, it spawned way too many enemies with default settings
+        # I tried scaling the image, but it seemed to mess with the rect way more, so I just scaled the height/width between the spawns
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (90, 90, 90)
 
         # Ship settings
         self.ship_speed = 3.0
+        self.ship_limit = 3
 
         # Bullet settings
         self.bullet_speed = 5.0
@@ -18,3 +21,9 @@ class Settings:
         self. bullet_height = 15
         self.bullet_color = (230, 230, 230)
         self.bullets_allowed = 5
+        
+        # Alien settings
+        self.alien_speed = 1.5
+        self.fleet_drop_speed = 15
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
